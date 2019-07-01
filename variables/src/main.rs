060bc143648a90af5eead9c311b4791d8f6c9932
Kristian Wahlroos <kjhwah@utu.fi>
2019-07-01 15:06:40 +0300

    3_1

---
+++ variables/src/main.rs
@@ -0,0 +1,9 @@
+fn main() {
+    let x = 5;
+    println!("The vlaue of x is {}", x);
+    let x = x + 1;
+    println!("The vlaue of x is {}", x);
+    let x = x * 2;
+    println!("The vlaue of x is {}", x);
+
+}
