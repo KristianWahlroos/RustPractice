3e2ffcb788221113ee1289b124de4a44260902fc
Kristian Wahlroos <kjhwah@utu.fi>
2019-07-08 18:24:17 +0300

    rectangles

---
+++ rectangles/src/main.rs
@@ -0,0 +1,25 @@
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+
+    fn can_hold (&self, other: &Rectangle) -> bool{
+        self.width > other.width && self.height > other.height
+    }
+}
+
+fn main() {
+    let rect1 = Rectangle {width: 30, height: 50};
+
+    println!("The are of the rectangle is {} square pixels.", rect1.area() );
+
+}
+
+fn area(rectangle: &Rectangle) -> u32{
+    rectangle.width * rectangle.height
+}
